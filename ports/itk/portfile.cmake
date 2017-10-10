8b3bd0f4799fc40b283243a48a2438a55f825838
Dženan Zukić <dzenan.zukic@kitware.com>
2017-10-10 09:48:18 -0400

    Builds, but post-install check fails due to presence of itkTestDriver.exe

--- ports/itk/portfile.cmake
+++ ports/itk/portfile.cmake
@@ -10,14 +10,6 @@ vcpkg_extract_source_archive(${ARCHIVE})
 # directory path length needs to be shorter than 50 characters
 file(RENAME ${CURRENT_BUILDTREES_DIR}/src/InsightToolkit-4.12.0 ${SOURCE_PATH})
 
-# if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    # # HACK: The FindHDF5.cmake script does not seem to detect the HDF5_DEFINITIONS correctly
-    # #       if HDF5 has been built without the tools (which is the case in the HDF5 port),
-    # #       so we set the BUILT_AS_DYNAMIC_LIB=1 flag here explicitly because we know HDF5
-    # #       has been build as dynamic library in the current case.
-    # list(APPEND ADDITIONAL_OPTIONS "-DHDF5_DEFINITIONS=-DH5_BUILT_AS_DYNAMIC_LIB=1")
-# endif()
-
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
@@ -30,7 +22,7 @@ vcpkg_configure_cmake(
         # -DITK_WRAP_PYTHON=ON
         # -DITK_PYTHON_VERSION=3
         -DITK_USE_SYSTEM_EXPAT=ON
-        -DITK_USE_SYSTEM_HDF5=OFF # HDF5 is problematic
+        -DITK_USE_SYSTEM_HDF5=ON # HDF5 is problematic
         -DITK_USE_SYSTEM_JPEG=ON
         # -DITK_USE_SYSTEM_LIBRARIES=ON
         -DITK_USE_SYSTEM_PNG=ON
@@ -40,16 +32,19 @@ vcpkg_configure_cmake(
         -DITK_INSTALL_DOC_DIR=share/itk/doc
         -DITK_INSTALL_PACKAGE_DIR=share/itk
         -DITK_FORBID_DOWNLOADS=OFF
+        -DITK_BUILD_DEFAULT_MODULES=OFF # turns on HDF5, which is problematic
+        -DITKGroup_IO=OFF # turns on HDF5, which is problematic
+        -DModule_ITKIOMesh=ON
+        -DModule_ITKIOCSV=ON
+        -DITKGroup_Filtering=ON
+        -DITKGroup_Registration=ON
+        -DITKGroup_Segmentation=ON
         -DModule_IOSTL=ON #example how to turn on a non-default module
-        -DModule_ITKReview=ON
+        -DModule_ITKReview=OFF
         -DModule_ITKVtkGlue=ON # this option requires VTK
+        -DModule_MorphologicalContourInterpolation=ON # example how to turn on a remote module
+        -DModule_RLEImage=ON
         ${ADDITIONAL_OPTIONS}
-    # OPTIONS_RELEASE
-        # -DHDF5_C_LIBRARY=${CURRENT_INSTALLED_DIR}/lib/hdf5.lib
-        # -DHDF5_C_HL_LIBRARY=${CURRENT_INSTALLED_DIR}/lib/hdf5_hl.lib
-    # OPTIONS_DEBUG
-        # -DHDF5_C_LIBRARY=${CURRENT_INSTALLED_DIR}/debug/lib/hdf5_D.lib
-        # -DHDF5_C_HL_LIBRARY=${CURRENT_INSTALLED_DIR}/debug/lib/hdf5_hl_D.lib
 )
 
 vcpkg_install_cmake()
